4a2c504a015faf8e129aee892eaff19d4af186cb
jannesvh <jvanhelsdingen@retrorabbit.co.za>
2022-01-24 13:36:51 +0200

    added s3 upload

---
+++ GradDemo.Api/Controllers/S3Controller.cs
@@ -0,0 +1,29 @@
+﻿using GradDemo.Api.Models;
+using GradDemo.Api.Providers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+
+namespace GradDemo.Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class S3Controller : ControllerBase
+    {
+        private IConfiguration _configuration;
+
+        public S3Controller(IConfiguration iConfig)
+        {
+            _configuration = iConfig;
+        }
+
+        [HttpGet("UploadFile")]
+        public async Task<Response<string>> UploadFileAsync()
+        {
+            await S3Provider.UploadFileAsync(_configuration.GetValue<string>("AWS:S3Bucket"));
+
+            return Response<string>.Error("TODO");
+        }
+    }
+}
---
+++ GradDemo.Api/Providers/S3Provider.cs
@@ -0,0 +1,29 @@
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace GradDemo.Api.Providers
+{
+    public class S3Provider
+    {
+        public static async Task UploadFileAsync(string bucket)
+        {
+            var client = new AmazonS3Client();
+
+            FileInfo file = new FileInfo(@".\imgs\ball.jpg");
+            string path = "demo/ball3.jpg";
+
+            PutObjectRequest request = new PutObjectRequest()
+            {
+                InputStream = file.OpenRead(),
+                BucketName = bucket,
+                Key = path // <-- in S3 key represents a path  
+            };
+
+            PutObjectResponse response = await client.PutObjectAsync(request);
+
+            //TODO Response
+        }
+    }
+}
